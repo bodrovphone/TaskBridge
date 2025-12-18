bc26820f4284d13800b639f80913544e8402b32c
Alex Bodrov <bodrovphone@gmail.com>
2025-12-18 17:55:30 +0200

    uncommented Facebook auth btn

--- src/components/ui/auth-slide-over.tsx
+++ src/components/ui/auth-slide-over.tsx
@@ -478,24 +478,23 @@ export default function AuthSlideOver({ isOpen, onClose, action }: AuthSlideOver
          </NextUIButton>
         )}
 
-        {/* Facebook Login Button - HIDDEN until app is in Live Mode */}
-        {/* @todo BEFORE-PROD: Uncomment when Facebook app is switched to Live Mode */}
-        {/* See: todo_tasks/before-prod-release/facebook-oauth-live-mode.md */}
-        {/* Note: Also add !webViewInfo.isWebView check when enabling */}
-        {/* <NextUIButton
-         variant="bordered"
-         size="lg"
-         className="w-full"
-         onPress={handleFacebookAuth}
-         isDisabled={isLoading}
-         startContent={
-          <svg className="w-5 h-5" viewBox="0 0 24 24" fill="#1877F2">
-           <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-          </svg>
-         }
-        >
-         {t('auth.continueWith')} Facebook
-         </NextUIButton> */}
+        {/* Facebook Login Button */}
+        {!webViewInfo.isWebView && (
+         <NextUIButton
+          variant="bordered"
+          size="lg"
+          className="w-full"
+          onPress={handleFacebookAuth}
+          isDisabled={isLoading}
+          startContent={
+           <svg className="w-5 h-5" viewBox="0 0 24 24" fill="#1877F2">
+            <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+           </svg>
+          }
+         >
+          {t('auth.continueWith')} Facebook
+         </NextUIButton>
+        )}
 
          {/* Telegram Login Button - COMMENTED OUT - Will be re-enabled later */}
          {/* <div className="w-full flex flex-col items-center gap-2">
